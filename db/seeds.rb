ec587249436dd205311451cfe57a6679b33f5111
shly <1028442429@qq.com>
2013-02-19 14:20:12 +0800

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+sorts = Sort.create([{ name: 'kai xin'},{ name: "cidian"}])
